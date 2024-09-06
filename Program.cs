43f51a4b9ea3b0b11124aa7249dc32c7f03f3a78
Duck-sot <raja07001@utb.vaxjo.se>
2024-09-06 13:58:08 +0200

    Hella uppgiften samt start på två del 2

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿/*Console.WriteLine("give me a number bettwen 1 and 100");
+string input = Console.ReadLine();
+for( int tal = int.Parse(input); tal<=101; tal++) {
+    Console.Write(tal + "  ");
+    if(tal >=101){
+    Console.WriteLine(",");
+    }
+
+}*/
+int antalTal;
+double summa = 0 , input;
+ 
