f587ac61b2d3ac35b35beb0338f29f1b47d45768
Duck-sot <raja07001@utb.vaxjo.se>
2024-09-11 11:13:08 +0200

    jag la till en del av up 2

--- Program.cs
+++ Program.cs
@@ -7,6 +7,14 @@ for( int tal = int.Parse(input); tal<=101; tal++) {
     }
 
 }*/
-int antalTal;
+double small =0; 
+double Big =0; 
 double summa = 0 , input;
- 
+int antal= int.Parse(Console.ReadLine());
+for(int i =0 ; i<5 ; i++){
+    Console.WriteLine("skrive tal" + i + ":" );
+    input = Convert.ToDouble(Console.ReadLine());
+    summa = summa + input;
+}
+Console.WriteLine( "medelvärdet är" + (summa / antal) );
+
--- obj/Debug/net8.0/Loppar--v.AssemblyInfo.cs
+++ obj/Debug/net8.0/Loppar--v.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Loppar--v")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43f51a4b9ea3b0b11124aa7249dc32c7f03f3a78")]
 [assembly: System.Reflection.AssemblyProductAttribute("Loppar--v")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Loppar--v")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
